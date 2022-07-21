ab0c5a18890358f6b2805ca31be440e452687ef9
Guilherme-Ciano <gui_ciano31@yahoo.com>
2022-07-20 21:10:28 -0300

    add: some new projects ideas & route easter egg

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const Express = require("express");
 const CORS = require("cors");
 const app = Express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(CORS());
 
 app.use("/random-numbers", require("./routes/randomNumbers"));
 app.use("/random-projects", require("./routes/randomProjects"));
+
+app.get("/", (req, res) => {
+  res.send("Wrong path my boi, try /random-numbers or /random-projects");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}\nhttp://localhost:${port}`);
