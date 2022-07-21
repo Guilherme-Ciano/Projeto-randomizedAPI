5115a30f9f9b0f4ff86bc3edcb4a6872dd34475e
Guilherme-Ciano <gui_ciano31@yahoo.com>
2022-07-20 22:59:26 -0300

    cahnge port declaration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Express = require("express");
 const CORS = require("cors");
 const app = Express();
 
+const port = process.env.PORT || 3000;
+
 app.use(CORS());
 
 app.use("/random-numbers", require("./routes/randomNumbers"));
@@ -11,6 +13,6 @@ app.get("/", (req, res) => {
   res.send("Wrong path my boi, try /random-numbers or /random-projects");
 });
 
-app.listen(8080, () => {
-  console.log(`Listening on port 8080\nhttp://localhost:8080`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}\nhttp://localhost:${port}`);
 });
